ee8b4f1148ab66a8614b2d9eae124e1cc285a390
JiaEN <qwas564779@gmail.com>
2024-01-29 21:00:40 +0800

    加入專案檔案。

---
+++ MIST155/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIST155.Models;
+
+namespace MIST155.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(x => x.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
---
+++ MIST155/Controllers/HomeworkController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MIST155.Models;
+namespace MIST155.Controllers
+{
+    public class HomeworkController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
